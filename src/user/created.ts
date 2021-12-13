0be10a52bf580118f75dc15503284a38fc949ec4
Ken Mueller <kenmueller0@gmail.com>
2021-12-13 14:35:03 -0800

    log creation time

--- src/user/created.ts
+++ src/user/created.ts
@@ -1,15 +1,18 @@
 import { auth } from 'firebase-functions'
-import { getFirestore } from 'firebase-admin/firestore'
+import { getFirestore, FieldValue } from 'firebase-admin/firestore'
 
 import INITIAL_CASH from '../cash/initial'
 
 const firestore = getFirestore()
 
 const userCreated = auth.user().onCreate(async user => {
+	console.log(user.metadata.creationTime)
+
 	await firestore.doc(`users/${user.uid}`).create({
 		name: user.displayName ?? null,
 		email: user.email ?? null,
-		cash: INITIAL_CASH
+		cash: INITIAL_CASH,
+		created: FieldValue.serverTimestamp()
 	})
 })
 

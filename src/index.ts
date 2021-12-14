0431ca935fa6528cc9d5e776835ab6d846a234f7
Ken Mueller <kenmueller0@gmail.com>
2021-12-13 19:21:42 -0800

    export company triggers

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import './initialize'
 
 export { default as userCreated } from './user/created'
 export { default as userDeleted } from './user/deleted'
+
+export { default as companyCreated } from './company/created'
+export { default as companyUpdated } from './company/updated'
+export { default as companyDeleted } from './company/deleted'
